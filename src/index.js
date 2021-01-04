8737f54d6abcfaf833bffb74acbbc3df08588cef
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2021-01-04 17:34:00 +0100

    Select between different maps.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,49 @@
 import L from 'leaflet';
 
-const map = L.map('map').setView([51.505, -0.09], 1).setView([65.505, 12], 5);;
+const capabilitiesUrl = "https://opencache.statkart.no/gatekeeper/gk/gk.open_wmts?request=GetCapabilities&service=WMS";
+const map = L.map('map').setView([65.505, 12], 5);
+let layer = null;
 
-// Layers:
-// - norgeskart_bakgrunn
-// - norgeskart_bakgrunn2
-// - topo4
-// - topo4graatone
-// - europa
-// - europa_forenklet
-// - kartdata3
-// - norges_grunnkart
-// - norges_grunnkart_graatone
-// - terreng_norgeskart
-// - vegnett ?
-// - sjokartraster
-// - toporaster4
-// - fjellskygge
+function setLayer(id) {
+    if (layer!==null) {
+        map.removeLayer(layer);
+        layer = null;
+    }
+    layer = L.tileLayer(
+        `http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=${id}&zoom={z}&x={x}&y={y}`,
+        {
+            maxZoom: 20,
+            attribution: '<a href="https://www.kartverket.no/">Kartverket</a>'
+        });    
+    layer.addTo(map);
+}
 
-L.tileLayer(
-    'http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}', {
-    attribution: '<a href="https://www.kartverket.no/">Kartverket</a>'
-    }).addTo(map);
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(capabilitiesUrl)
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            return Array.from(data.querySelectorAll("Layer")).map(layer => {
+                return {
+                    title: layer.querySelector("Title")?.textContent,
+                    id: layer.querySelector("Identifier")?.textContent,
+                    format: layer.querySelector("Format")?.textContent,
+                };
+            })}
+        )
+        .then(data => {
+            const elem = document.getElementById('selected-map');
+            console.log(elem);
+            data.forEach(item => {
+                const option = document.createElement("option");
+                option.textContent = item.title;
+                option.setAttribute("value", item.id);
+                elem.appendChild(option);
+            })
+
+            elem.addEventListener("change", evt => {
+                setLayer(elem.value);
+            });
+        })
+        ;
+})
