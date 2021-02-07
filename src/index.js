f4fb6b7df46cfb9b842ecda7ab08756508a929ef
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2021-02-07 12:41:20 +0100

    Bruk fast liste over kartlag.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import L from 'leaflet';
 
-const capabilitiesUrl = "https://opencache.statkart.no/gatekeeper/gk/gk.open_wmts?request=GetCapabilities&service=WMS";
 const map = L.map('map').setView([65.505, 12], 5);
-let layer = null;
 
+// Load all layers from WMTS capabilities.
+// NOTE: No filtering on tile matrix set is done, 
+// so not all maps can be shown in LeafletJS.
+function loadCapabilities() {
+    const capabilitiesUrl = "https://opencache.statkart.no/gatekeeper/gk/gk.open_wmts?request=GetCapabilities&service=WMS";
+    return fetch(capabilitiesUrl)
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            return Array.from(data.querySelectorAll("Layer")).map(layer => {
+                return {
+                    title: layer.querySelector("Title")?.textContent,
+                    id: layer.querySelector("Identifier")?.textContent,
+                    format: layer.querySelector("Format")?.textContent,
+                };
+            })}
+        )
+};
+
+// Layers from Kartverket that shows in LeafletJS without extra plugins
+const availableLayers = [
+    { id: 'norgeskart_bakgrunn', title: 'Norgeskart bakgrunn' },
+    { id: 'topo4', title: 'Topografisk norgeskart' },
+    { id: 'topo4graatone', title: 'Topografisk norgeskart (gråtone)' },
+    { id: 'europa', title: 'Europakart' },
+    { id: 'kartdata3', title: 'Kartdata 3' },
+    { id: 'norges_grunnkart', title: 'Norges Grunnkart' },
+    { id: 'norges_grunnkart_graatone', title: 'Norges Grunnkart (gråtone)' },
+    { id: 'egk', title: 'Europeiske grunnkart' },
+    { id: 'havbunn_grunnkart', title: 'Havbunn grunnkart' },
+    { id: 'terreng_norgeskart', title: 'Terreng norgeskart' },
+    { id: 'sjokartraster', title: 'Sjøkart - raster' },
+    { id: 'toporaster4', title: 'Topografisk raster' },
+    { id: 'fjellskygge', title: 'Fjellskygge' },
+    { id: 'bakgrunnskart_forenklet', title: 'Forenklet bakgrunnskart' },
+];
+
+
+let layer = null;
 function setLayer(id) {
     if (layer!==null) {
         map.removeLayer(layer);
@@ -20,31 +57,20 @@ function setLayer(id) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch(capabilitiesUrl)
-        .then(response => response.text())
-        .then(str => new DOMParser().parseFromString(str, "text/xml"))
-        .then(data => {
-            return Array.from(data.querySelectorAll("Layer")).map(layer => {
-                return {
-                    title: layer.querySelector("Title")?.textContent,
-                    id: layer.querySelector("Identifier")?.textContent,
-                    format: layer.querySelector("Format")?.textContent,
-                };
-            })}
-        )
-        .then(data => {
-            const elem = document.getElementById('selected-map');
-            console.log(elem);
-            data.forEach(item => {
-                const option = document.createElement("option");
-                option.textContent = item.title;
-                option.setAttribute("value", item.id);
-                elem.appendChild(option);
-            })
+    const selectedMapElement = document.getElementById('selected-map');
+    selectedMapElement.addEventListener("change", evt => {
+        setLayer(selectedMapElement.value);
+    });
 
-            elem.addEventListener("change", evt => {
-                setLayer(elem.value);
-            });
+    function setOptions(data) {
+        data.forEach(item => {
+            const option = document.createElement("option");
+            option.textContent = item.title;
+            option.setAttribute("value", item.id);
+            selectedMapElement.appendChild(option);
         })
-        ;
+    }
+
+    setOptions(availableLayers);
+//    loadCapabilities().then(setOptions);
 })
