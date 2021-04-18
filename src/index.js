253c4a3128e564091bffa30ecfb334b137f8f11b
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2021-04-18 19:11:56 +0200

    Add location tracking.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import L from 'leaflet';
+import DefaultIcon from 'leaflet';
 
 const map = L.map('map').setView([65.505, 12], 5);
 
@@ -37,6 +38,39 @@ function setLayer(id) {
     layer.addTo(map);
 }
 
+
+let marker = null;
+function updatePosition(success) {
+    const pos = success.coords;
+    const latLng = success.latlng;
+    if (layer) {
+        if (!marker) {
+            marker = L.circleMarker(latLng, {radius: 5});
+            map.setView(latLng, 14);
+            marker.addTo(map);
+        }
+        else {
+            marker.setLatLng(latLng);
+        }
+    }
+}
+
+function startLocationTracking() {
+    map.locate({
+        watch: true,
+        enableHighAccuracy: true,
+    });
+    map.on('locationfound', updatePosition);
+}
+
+function stopLocationTracking() {
+    map.stopLocate();
+    if (marker) {
+        map.removeLayer(marker);
+        marker = null;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const selectedMapElement = document.getElementById('selected-map');
     selectedMapElement.addEventListener("change", evt => {
@@ -53,4 +87,26 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     setOptions(availableLayers);
+
+    var location = L.control({position: 'bottomleft'});
+    location.onAdd = function (map) {
+        const container = this._div = L.DomUtil.create('div', 'location');
+        this._div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>';
+
+        this._div.addEventListener('click', function() {
+            if(L.DomUtil.hasClass(container, 'selected')) {
+                stopLocationTracking();
+                L.DomUtil.removeClass(container, 'selected');
+            }
+            else {
+                if (layer) {
+                        startLocationTracking();
+                        L.DomUtil.addClass(container, 'selected');
+                }
+            }
+        });
+        return this._div;
+    }
+
+    location.addTo(map);
 })
